e8ece44fa91a010864b7515267b4160da8b5db0f
kourain <maiquyen16503@gmail.com>
2025-11-07 13:50:13 +0700

    tts

--- TTS/tts.py
+++ TTS/tts.py
@@ -1,3 +1,15 @@
-# Load model directly
-from transformers import AutoModel
-model = AutoModel.from_pretrained("capleaf/viXTTS", torch_dtype="auto")
+
+import os
+from huggingface_hub import hf_hub_download, snapshot_download
+
+
+os.system("python -m unidic download")
+print(" > Tải mô hình...")
+snapshot_download(repo_id="thinhlpg/viXTTS",
+                  repo_type="model",
+                  local_dir="model")
+hf_hub_download(
+    repo_id="coqui/XTTS-v2",
+    filename="speakers_xtts.pth",
+    local_dir="model",
+)
